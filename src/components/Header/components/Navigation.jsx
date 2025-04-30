f12322df51ba6af4c2165264d01e849e23ab38d4
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-30 17:39:21 -0300

    feat: implementa componente Navigation

---
+++ src/components/Header/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import "./Navigation.css";
+
+export default function Navigation() {
+  return (
+    <div className="navigation">
+      <p className="navigation__home">Home</p>
+      <button className="navigation__sign-in">Sign in</button>
+    </div>
+  );
+}
