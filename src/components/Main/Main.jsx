77b75f052e95970e7ee52fce4a416b074efc9eb7
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-01 15:26:30 -0300

    feat: implementa componente Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import Search from "./components/Search/Search";
+import About from "./components/About/About";
+
+export default function Main() {
+  return (
+    <main className="content">
+      <Search />
+      <About />
+    </main>
+  );
+}
