956fd6774d3b5dfb1d0b1f52606aafb3e20302c2
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-01 14:23:54 -0300

    feat: implementa componente Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import "./Footer.css";
+import githubIcon from "../../images/github-icon.png";
+import facebookIcon from "../../images/facebook-icon.png";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__copyrights">
+        &copy; 2025 Supersite, Powered by News API
+      </p>
+      <div className="footer__navigation">
+        <p className="footer__navigation-home">Home</p>
+        <p className="footer__navigation-site">Practicum by Yandex</p>
+        <div className="footer__navigation-icons">
+          <img
+            className="footer__navigation-icon"
+            src={githubIcon}
+            alt="Ícone do GitHub"
+          />
+          <img
+            className="footer__navigation-icon"
+            src={facebookIcon}
+            alt="Ícone do Facebook"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
