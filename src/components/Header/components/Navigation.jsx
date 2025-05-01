82925f2e324ee739f38f1c2b17047fd2012258bf
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-01 15:25:31 -0300

    feat: implementa componente Navigation

--- src/components/Header/components/Navigation.jsx
+++ src/components/Header/components/Navigation.jsx
@@ -1,10 +1,37 @@
 import "./Navigation.css";
+import logoutIcon from "../../../images/logout-icon.png";
+import { useState } from "react";
 
 export default function Navigation() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
+
+  // Função criada apenas para ajudar no desenvolvimento. Deve ser excluída ao final do desenvolvimento.
+  function handleUserLogin() {
+    setIsUserLoggedIn(!isUserLoggedIn);
+  }
+
   return (
     <div className="navigation">
-      <p className="navigation__home">Home</p>
-      <button className="navigation__sign-in">Sign in</button>
+      {isUserLoggedIn ? (
+        <>
+          <p className="navigation__link">Home</p>
+          <button onClick={handleUserLogin} className="navigation__button">
+            Sign in
+          </button>
+        </>
+      ) : (
+        <>
+          <p className="navigation__link">Home</p>
+          <p className="navigation__link">Saved articles</p>
+          <button
+            onClick={handleUserLogin}
+            className="navigation__button navigation__button_active"
+          >
+            User
+            <img className="navigation__button-image" src={logoutIcon} alt="" />
+          </button>
+        </>
+      )}
     </div>
   );
 }
