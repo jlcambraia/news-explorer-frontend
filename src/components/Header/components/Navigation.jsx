13af8dc6284cb8eeac055f48b773e26d6d64bd39
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-05 16:58:41 -0300

    feat: cria renderização dinâmica do navigation para página saved-news

--- src/components/Header/components/Navigation.jsx
+++ src/components/Header/components/Navigation.jsx
@@ -1,28 +1,61 @@
 import "./Navigation.css";
 import logoutIcon from "../../../images/logout-icon.png";
-import { useState } from "react";
+import logoutIconBlack from "../../../images/logout-icon-black.png";
+import { useState, useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { CurrentPathContext } from "../../../contexts/CurrentPathContext";
 
 export default function Navigation() {
   // isUserLoggedIn criado apenas para desenvolvimento. Passar para App no final do desenvolvimento do front-end.
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
+  const pathLocation = useContext(CurrentPathContext);
+
+  const handleActiveLink = ({ isActive }) =>
+    (pathLocation
+      ? "navigation__link"
+      : "navigation__link navigation__link_black") +
+    (isActive
+      ? pathLocation
+        ? " navigation__link_active"
+        : " navigation__link_active navigation__link_active_black"
+      : "");
 
   return (
     <div className="navigation">
-      {isUserLoggedIn ? (
+      {!isUserLoggedIn ? (
         <>
-          <p className="navigation__link">Home</p>
-          <button className="navigation__button">Sign in</button>
+          <NavLink className={handleActiveLink} to="/">
+            Home
+          </NavLink>
+          <button
+            className={
+              pathLocation ? "navigation__button" : "navigation__button_black"
+            }
+          >
+            Sign in
+          </button>
         </>
       ) : (
         <>
-          <p className="navigation__link">Home</p>
-          <p className="navigation__link">Saved articles</p>
+          <NavLink className={handleActiveLink} to="/">
+            Home
+          </NavLink>
+          <NavLink className={handleActiveLink} to="/saved-news">
+            Saved articles
+          </NavLink>
           <button
-            onClick={handleUserLogin}
-            className="navigation__button navigation__button_active"
+            className={
+              pathLocation
+                ? "navigation__button navigation__button_active"
+                : "navigation__button navigation__button_black navigation__button_active navigation__button_active_black"
+            }
           >
             User
-            <img className="navigation__button-image" src={logoutIcon} alt="" />
+            <img
+              className="navigation__button-image"
+              src={pathLocation ? logoutIcon : logoutIconBlack}
+              alt=""
+            />
           </button>
         </>
       )}
