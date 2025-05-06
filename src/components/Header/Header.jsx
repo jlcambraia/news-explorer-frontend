2a179652c98be1399acc5a58f8fb6131e0f0629c
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-06 16:27:29 -0300

    feat: redireciona usuário para "/" após clicar na logo da página

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,23 +2,26 @@ import "./Header.css";
 
 import { useContext } from "react";
 import { CurrentPathContext } from "../../contexts/CurrentPathContext";
+import { useNavigate } from "react-router-dom";
 
 import Navigation from "./components/Navigation";
 
-export default function Header() {
+export default function Header({ openPopup, loginPopup }) {
   const pathLocation = useContext(CurrentPathContext);
+  const navigate = useNavigate();
 
   return (
     <>
       <header className={pathLocation ? "header" : "header header_black"}>
         <h1
+          onClick={() => navigate("/")}
           className={
             pathLocation ? "header__logo" : "header__logo header__logo_black"
           }
         >
           NewsExplorer
         </h1>
-        <Navigation />
+        <Navigation openPopup={openPopup} loginPopup={loginPopup} />
       </header>
     </>
   );
