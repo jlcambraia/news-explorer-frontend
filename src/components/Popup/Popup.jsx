b476ddc63e2d79ba1c02c8dcf685beeb27d8b0a2
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-06 16:24:50 -0300

    feat: cria componente Popup

---
+++ src/components/Popup/Popup.jsx
@@ -0,0 +1,67 @@
+import "./Popup.css";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function Popup({
+  children,
+  title,
+  closePopup,
+  openPopup,
+  registerPopup,
+  loginPopup,
+}) {
+  useEffect(() => {
+    const handleEscClose = (evt) => {
+      if (evt.key === "Escape") {
+        if (document.activeElement) {
+          document.activeElement.blur();
+        }
+        closePopup();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscClose);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscClose);
+    };
+  }, [closePopup]);
+
+  const handleOverlayClick = (evt) => {
+    if (evt.target.classList.contains("popup")) {
+      closePopup();
+    }
+  };
+  return (
+    <div className="popup" onClick={handleOverlayClick}>
+      <div className="popup__container">
+        <button onClick={closePopup} className="popup__close-button"></button>
+        <h2 className="popup__title">{title}</h2>
+        {children}
+
+        {title === "Sign in" ||
+        title === "Registration failed, please try again!" ? (
+          <p className="popup__signup-text">
+            or{" "}
+            <Link
+              onClick={() => openPopup(registerPopup)}
+              className="popup__signup-link"
+            >
+              Sign up
+            </Link>
+          </p>
+        ) : (
+          <p className="popup__signup-text">
+            or{" "}
+            <Link
+              onClick={() => openPopup(loginPopup)}
+              className="popup__signup-link"
+            >
+              Sign in
+            </Link>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
