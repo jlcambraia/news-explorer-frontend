9c7c76b11b110da0465392b23be6458b06ff4fab
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-06 16:24:15 -0300

    feat: passa artigos fictícios criados apenas para desenvolvimento como props para SavednewsHeader e NewsCardList

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,12 +1,12 @@
 import SavedNewsHeader from "./components/SavedNewsHeader/SavedNewsHeader";
 import NewsCardList from "../Main/components/NewsCardList/NewsCardList";
 
-export default function SavedNews({ location }) {
+export default function SavedNews({ savedArticles, placeholder }) {
   return (
     <>
       <main>
-        <SavedNewsHeader />
-        <NewsCardList />
+        <SavedNewsHeader savedArticles={savedArticles} />
+        <NewsCardList savedArticles={savedArticles} placeholder={placeholder} />
       </main>
     </>
   );
