41b45a2ad46523ea6871e19c96e23e5e7e8036e3
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-07 10:32:56 -0300

    feat: deixa ícones e links do footer funcionais

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,54 @@
 import "./Footer.css";
 import githubIcon from "../../images/github-icon.png";
-import facebookIcon from "../../images/facebook-icon.png";
+import linkedinIcon from "../../images/linkedin-icon.png";
+
+import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
+  const navigate = useNavigate();
+
   return (
     <footer className="footer">
       <p className="footer__copyrights">
-        &copy; 2025 Supersite, Powered by News API
+        &copy; 2025 João Luiz Cambraia, Powered by News API
       </p>
       <div className="footer__navigation">
-        <p className="footer__navigation-link">Home</p>
-        <p className="footer__navigation-link">TripleTen</p>
+        <p onClick={() => navigate("/")} className="footer__navigation-link">
+          Home
+        </p>
+        <a
+          className="footer__navigation-link"
+          href="https://tripleten.com/pt-bra/"
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          TripleTen
+        </a>
         <div className="footer__navigation-icons">
-          <img
-            className="footer__navigation-icon"
-            src={githubIcon}
-            alt="Ícone do GitHub"
-          />
-          <img
-            className="footer__navigation-icon"
-            src={facebookIcon}
-            alt="Ícone do Facebook"
-          />
+          <a
+            className="footer__navigation-anchor"
+            href="https://github.com/jlcambraia/"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
+            <img
+              className="footer__navigation-icon"
+              src={githubIcon}
+              alt="Ícone do GitHub"
+            />
+          </a>
+          <a
+            className="footer__navigation-anchor"
+            href="https://www.linkedin.com/in/joaoluizcambraia/"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
+            <img
+              className="footer__navigation-icon"
+              src={linkedinIcon}
+              alt="Ícone do Facebook"
+            />
+          </a>
         </div>
       </div>
     </footer>
