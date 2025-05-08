8cbd36288d6c75778b244b23e258fa06944271bd
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-08 16:31:13 -0300

    feat: recebe e repassa props

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,12 +1,22 @@
 import SavedNewsHeader from "./components/SavedNewsHeader/SavedNewsHeader";
 import NewsCardList from "../Main/components/NewsCardList/NewsCardList";
 
-export default function SavedNews({ savedArticles, placeholder }) {
+export default function SavedNews({
+  savedArticles,
+  placeholder,
+  isUserLoggedIn,
+  isSearchingForNews,
+}) {
   return (
     <>
       <main>
         <SavedNewsHeader savedArticles={savedArticles} />
-        <NewsCardList savedArticles={savedArticles} placeholder={placeholder} />
+        <NewsCardList
+          savedArticles={savedArticles}
+          placeholder={placeholder}
+          isUserLoggedIn={isUserLoggedIn}
+          isSearchingForNews={isSearchingForNews}
+        />
       </main>
     </>
   );
