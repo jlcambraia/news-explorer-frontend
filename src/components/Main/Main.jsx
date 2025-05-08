33b18ffd0f1fd6dd2b82fb2923a357d62266375e
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-08 16:29:14 -0300

    feat: recebe e repassa props

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,22 @@ import Search from "./components/Search/Search";
 import About from "./components/About/About";
 import NewsCardList from "./components/NewsCardList/NewsCardList";
 
-export default function Main({ placeholder, savedArticles }) {
+export default function Main({
+  placeholder,
+  savedArticles,
+  isUserLoggedIn,
+  isSearchingForNews,
+}) {
   return (
     <main>
       <Search />
+      <NewsCardList
+        placeholder={placeholder}
+        savedArticles={savedArticles}
+        isUserLoggedIn={isUserLoggedIn}
+        isSearchingForNews={isSearchingForNews}
+      />
       <About />
-      <NewsCardList placeholder={placeholder} savedArticles={savedArticles} />
     </main>
   );
 }
