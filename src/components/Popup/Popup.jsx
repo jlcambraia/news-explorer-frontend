f0a914cf3f3c5249056d31e49b5280cbe5612f27
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-08 16:31:04 -0300

    feat: melhora funcionamento de fechar o popup clicando fora dele

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -21,46 +21,51 @@ export default function Popup({
     };
 
     document.addEventListener("keydown", handleEscClose);
-
     return () => {
       document.removeEventListener("keydown", handleEscClose);
     };
   }, [closePopup]);
 
   const handleOverlayClick = (evt) => {
-    if (evt.target.classList.contains("popup")) {
+    if (
+      evt.target.classList.contains("popup") ||
+      evt.target.classList.contains("popup__wrapper")
+    ) {
       closePopup();
     }
   };
+
   return (
     <div className="popup" onClick={handleOverlayClick}>
-      <div className="popup__container">
-        <button onClick={closePopup} className="popup__close-button"></button>
-        <h2 className="popup__title">{title}</h2>
-        {children}
+      <div className="popup__wrapper" onClick={handleOverlayClick}>
+        <div className="popup__container">
+          <button onClick={closePopup} className="popup__close-button" />
+          <h2 className="popup__title">{title}</h2>
+          {children}
 
-        {title === "Sign in" ||
-        title === "Registration failed, please try again!" ? (
-          <p className="popup__signup-text">
-            {title === "Sign in" ? "or " : null}
-            <Link
-              onClick={() => openPopup(registerPopup)}
-              className="popup__signup-link"
-            >
-              Sign up
-            </Link>
-          </p>
-        ) : (
-          <p className="popup__signup-text">
-            {title === "Sign up" ? "or " : null}
-            <Link
-              onClick={() => openPopup(loginPopup)}
-              className="popup__signup-link"
-            >
-              Sign in
-            </Link>
-          </p>
-        )}
+          {title === "Sign in" ||
+          title === "Registration failed, please try again!" ? (
+            <p className="popup__signup-text">
+              {title === "Sign in" ? "or " : null}
+              <Link
+                onClick={() => openPopup(registerPopup)}
+                className="popup__signup-link"
+              >
+                Sign up
+              </Link>
+            </p>
+          ) : (
+            <p className="popup__signup-text">
+              {title === "Sign up" ? "or " : null}
+              <Link
+                onClick={() => openPopup(loginPopup)}
+                className="popup__signup-link"
+              >
+                Sign in
+              </Link>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
