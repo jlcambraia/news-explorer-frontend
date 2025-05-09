3688fd8bc43712ab5bedc326b2e774e4e7de0be4
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-09 14:42:38 -0300

    feat: utiliza validator para os keywords

--- src/pages/SavedNews/components/SavedNewsHeader/SavedNewsHeader.jsx
+++ src/pages/SavedNews/components/SavedNewsHeader/SavedNewsHeader.jsx
@@ -1,69 +1,29 @@
 import "./SavedNewsHeader.css";
+import {
+  extractUniqueKeywords,
+  formatKeywordsList,
+} from "../../../../utils/validators/keywords";
 
 export default function SavedNewsHeader({ savedArticles }) {
-  const keywords = [];
-
-  savedArticles.forEach((article) => {
-    if (!keywords.includes(article.source.name)) {
-      keywords.push(article.source.name);
-    }
-  });
-
-  function showSavedArticlesKeywords() {
-    if (keywords.length === 0) {
-      return null;
-    }
-    if (keywords.length === 1) {
-      return (
-        <>
-          <span>By keywords: </span>{" "}
-          <span className="saved-news-header__keyword">{keywords[0]}</span>
-        </>
-      );
-    }
-    if (keywords.length === 2) {
-      return (
-        <>
-          <span>By keywords: </span>
-          <span className="saved-news-header__keyword">
-            {keywords[0]}, {keywords[1]}
-          </span>
-        </>
-      );
-    }
-    if (keywords.length === 3) {
-      return (
-        <>
-          <span>By keywords: </span>
-          <span className="saved-news-header__keyword">
-            {keywords[0]}, {keywords[1]}, {keywords[2]}
-          </span>
-        </>
-      );
-    }
-
-    return (
-      <>
-        <span>By keywords: </span>
-        <span className="saved-news-header__keyword">
-          {keywords[0]}, {keywords[1]}, and {keywords.length - 2} other
-        </span>
-      </>
-    );
-  }
+  const keywords = extractUniqueKeywords(savedArticles);
 
   return (
-    <>
-      <div className="saved-news-header">
-        <p className="saved-news-header__breadcrumb">Saved articles</p>
-        <h2 className="saved-news-header__title">
-          Elise, you have {savedArticles.length} saved{" "}
-          {keywords.length === 1 ? "article" : "articles"}
-        </h2>
-        <p className="saved-news-header__keywords">
-          {showSavedArticlesKeywords()}
-        </p>
-      </div>
-    </>
+    <div className="saved-news-header">
+      <p className="saved-news-header__breadcrumb">Saved articles</p>
+      <h2 className="saved-news-header__title">
+        Elise, you have {savedArticles.length} saved{" "}
+        {keywords.length === 1 ? "article" : "articles"}
+      </h2>
+      <p className="saved-news-header__keywords">
+        {keywords.length > 0 && (
+          <>
+            <span>By keywords: </span>
+            <span className="saved-news-header__keyword">
+              {formatKeywordsList(keywords)}
+            </span>
+          </>
+        )}
+      </p>
+    </div>
   );
 }
