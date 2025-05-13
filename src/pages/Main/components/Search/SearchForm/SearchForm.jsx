f47a5d92d7373e6cc7cdd9a930abbbbe16c5dcc4
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-13 11:34:54 -0300

    feat: implementa funcionalidade de buscar artigos na api via input e submit

--- src/pages/Main/components/Search/SearchForm/SearchForm.jsx
+++ src/pages/Main/components/Search/SearchForm/SearchForm.jsx
@@ -1,12 +1,28 @@
 import "./SearchForm.css";
 
-export default function SearchForm() {
+import { useState } from "react";
+
+export default function SearchForm({ searchNewsFromApi }) {
+  const [searchInputValue, setSearchInputValue] = useState("");
+
+  function handleSearchInputChange(evt) {
+    setSearchInputValue(evt.target.value.trim());
+  }
+
+  const handleSearchFormSubmit = (evt) => {
+    evt.preventDefault();
+    searchNewsFromApi(searchInputValue);
+    setSearchInputValue("");
+  };
+
   return (
-    <form className="search-form" action="submit">
+    <form className="search-form" onSubmit={handleSearchFormSubmit}>
       <input
         className="search-form__input"
         placeholder="Enter topic"
         type="text"
+        value={searchInputValue}
+        onChange={handleSearchInputChange}
       />
       <button className="search-form__button">Search</button>
     </form>
