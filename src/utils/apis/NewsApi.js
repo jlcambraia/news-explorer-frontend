7b74e138abd1db3d786b4eb90a1163a304322de6
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-13 10:10:55 -0300

    feat: cria classe Api para busca de artigos

---
+++ src/utils/apis/NewsApi.js
@@ -0,0 +1,43 @@
+class Api {
+  constructor({ baseUrl, token, makeRequest, fromDate, toDate }) {
+    this._baseUrl = baseUrl;
+    this._token = token;
+    this._makeRequest = makeRequest;
+    this._fromDate = fromDate;
+    this._toDate = toDate;
+  }
+
+  _handleServerResponse(res) {
+    return res.ok ? res.json() : Promise.reject(console.log(res.status));
+  }
+
+  searchForNews(search) {
+    return this._makeRequest(
+      `${this._baseUrl}q=${search}&from=${this._fromDate}&to=${this._toDate}&pageSize=100`,
+      {
+        headers: {
+          authorization: this._token,
+        },
+      }
+    ).then(this._handleServerResponse);
+  }
+}
+
+// Função que consegue data de N dias atrás
+function getDateFromNDaysAgo(n) {
+  const date = new Date();
+  date.setDate(date.getDate() - n);
+  return date.toISOString().slice(0, 10);
+}
+
+// Configuração para API
+const apiConfig = {
+  baseUrl: "https://newsapi.org/v2/everything?",
+  token: "a300df36ab4040ff821994aeec2479fe",
+  makeRequest: (...args) => fetch(...args),
+  fromDate: getDateFromNDaysAgo(7),
+  toDate: new Date().toISOString().slice(0, 10),
+};
+
+// Instância para o Api
+export const api = new Api(apiConfig);
