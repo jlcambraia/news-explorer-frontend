8011606676d0efd4e3fe82ddd04ea3811918bdb6
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-14 11:01:50 -0300

    feat: retira props que não é mais utilizada

--- src/pages/Main/components/Search/Search.jsx
+++ src/pages/Main/components/Search/Search.jsx
@@ -2,7 +2,7 @@ import "./Search.css";
 
 import SearchForm from "./SearchForm/SearchForm";
 
-export default function Search({ searchNewsFromApi }) {
+export default function Search() {
   return (
     <section className="search">
       <h2 className="search__title">What's going on in the world?</h2>
@@ -10,7 +10,7 @@ export default function Search({ searchNewsFromApi }) {
         Find the latest news on any topic and save them in your personal
         account.
       </p>
-      <SearchForm searchNewsFromApi={searchNewsFromApi} />
+      <SearchForm />
     </section>
   );
 }
