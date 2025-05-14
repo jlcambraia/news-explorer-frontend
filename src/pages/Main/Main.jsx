437c3c94412c4eb6c2d344a11882ba605d9080bf
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-14 10:54:36 -0300

    refactor: retira props que não são mais utilizadas

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,25 +2,13 @@ import Search from "./components/Search/Search";
 import About from "./components/About/About";
 import NewsCardList from "../../components/NewsCardList/NewsCardList";
 
-export default function Main({
-  savedArticles,
-  isUserLoggedIn,
-  isSearchingForNews,
-  searchNewsFromApi,
-  hasSearched,
-  searchError,
-  searchKeyword,
-}) {
+export default function Main({ savedArticles, isUserLoggedIn }) {
   return (
     <main>
-      <Search searchNewsFromApi={searchNewsFromApi} />
+      <Search />
       <NewsCardList
         savedArticles={savedArticles}
         isUserLoggedIn={isUserLoggedIn}
-        isSearchingForNews={isSearchingForNews}
-        hasSearched={hasSearched}
-        searchError={searchError}
-        searchKeyword={searchKeyword}
       />
       <About />
     </main>
