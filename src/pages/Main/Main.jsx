dfdfe38b18c37e0f1681a88dcd22cf6fa7bd8c95
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-14 16:25:17 -0300

    feat: retira prop savedArticles e passa função handleSaveArticle como prop

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,13 +2,13 @@ import Search from "./components/Search/Search";
 import About from "./components/About/About";
 import NewsCardList from "../../components/NewsCardList/NewsCardList";
 
-export default function Main({ savedArticles, isUserLoggedIn }) {
+export default function Main({ isUserLoggedIn, handleSaveArticle }) {
   return (
     <main>
       <Search />
       <NewsCardList
-        savedArticles={savedArticles}
         isUserLoggedIn={isUserLoggedIn}
+        handleSaveArticle={handleSaveArticle} // Será removido ou atualizado após desenvolvimento da Api correta.
       />
       <About />
     </main>
