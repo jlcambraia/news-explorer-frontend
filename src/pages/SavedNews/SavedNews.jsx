52184fa315f3bfb06825698df8586caa8d3a32d4
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-14 17:26:00 -0300

    feat: recebe e repassa props para abrir popup

--- src/pages/SavedNews/SavedNews.jsx
+++ src/pages/SavedNews/SavedNews.jsx
@@ -1,13 +1,18 @@
 import SavedNewsHeader from "./components/SavedNewsHeader/SavedNewsHeader";
 import NewsCardList from "../../components/NewsCardList/NewsCardList";
 
-export default function SavedNews({ isUserLoggedIn, handleRemoveArticle }) {
+export default function SavedNews({
+  isUserLoggedIn,
+  handleOpenPopup,
+  handleRemoveArticle,
+}) {
   return (
     <>
       <main>
         <SavedNewsHeader />
         <NewsCardList
           isUserLoggedIn={isUserLoggedIn}
+          handleOpenPopup={handleOpenPopup}
           handleRemoveArticle={handleRemoveArticle} // Será removido ou atualizado após desenvolvimento da Api correta.
         />
       </main>
