67b92f07eda8bea4a4233f264639fe83de3e4879
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-23 14:46:00 -0300

    feat: cria imputValidator

---
+++ src/utils/validators/inputValidator.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export function useInputValidator(initialValue = "") {
+  const [value, setValue] = useState(initialValue);
+  const [isValid, setIsValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleChange(evt) {
+    const input = evt.target;
+    const cleanValue = input.value.trim().replace(/\s+/g, "");
+
+    setValue(cleanValue);
+    setIsValid(input.validity.valid);
+    setErrorMessage(input.validationMessage);
+  }
+
+  return {
+    value,
+    isValid,
+    errorMessage,
+    handleChange,
+  };
+}
